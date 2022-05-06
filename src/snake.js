cab75e81a98aa5d3b63daa6b32dcee1846624059
a-gehlot <andrew.gehlot@gmail.com>
2022-05-06 14:13:33 -0400

    pivoting segments of snake

--- src/snake.js
+++ src/snake.js
@@ -9,39 +9,39 @@ class Snake {
     
     constructor(board) {
         this.direction = "N";
-        this.segments = [[10,10]];
+        this.segments = [[10,10], [11,10], [12,10], [13,10]];
         this.board = board;
     }
 
     move() {
-        let lastSeg = this.segments.pop();
         switch(this.direction) {
             case "N":
-                this.addSeg(lastSeg, [-1,0]);
+                this.shiftSeg([-1,0]);
                 break;
             case "S":
-                this.addSeg(lastSeg, [1,0]);
+                this.shiftSeg([1,0]);
                 break;
             case "E":
-                this.addSeg(lastSeg, [0,1]);
+                this.shiftSeg([0,1]);
                 break;
             case "W":
-                this.addSeg(lastSeg, [0, -1]);
+                this.shiftSeg([0, -1]);
                 break;
         }
     }
 
-    addSeg(lastSeg, dir) {
-        let i = lastSeg[0] + dir[0];
-        let j = lastSeg[1] + dir[1];
-        this.segments.push([i,j])
+    shiftSeg(dir) {
+        this.segments.pop();
+        let newHead = this.segments[0].map((num, idx) => {
+            return num + dir[idx]
+        })
+        this.segments.unshift(newHead);
     }
-
 
 
     turn(dir) {
         this.direction = dir;
-        console.log(this.dir);
+        console.log(dir)
     }
 
     inBounds() {
@@ -51,6 +51,11 @@ class Snake {
             }
         return true;
     }
+
+    addRearSeg() {
+
+    }
+    
 }
 
 module.exports = Snake;
