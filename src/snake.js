aa1cb77fbc3df1a58fd849ef0c30e2659aae0a0a
a-gehlot <andrew.gehlot@gmail.com>
2022-05-10 15:13:53 -0400

    not allowing snake to turn on self

--- src/snake.js
+++ src/snake.js
@@ -30,18 +30,47 @@ class Snake {
         }
     }
 
-    shiftSeg(dir) {
+    shiftSeg(dirCoord) {
         this.segments.pop();
         let newHead = this.segments[0].map((num, idx) => {
-            return num + dir[idx]
+            return num + dirCoord[idx]
         })
         this.segments.unshift(newHead);
     }
 
 
     turn(dir) {
+        if (this.isOpposite(dir)) {
+            return;
+        }
         this.direction = dir;
         console.log(dir)
+    }
+
+    isOpposite(dir) {
+        switch(this.direction) {
+            case "N":
+                if (dir === "S") {
+                    return true;
+                }
+                break;
+            case "S":
+                if (dir === "N") {
+                    return true;
+                }
+                break;
+            case "E":
+                if (dir === "W") {
+                    return true;
+                }
+                break;
+            case "W":
+                if (dir === "E") {
+                    return true;
+                }
+                break;
+        }   
+        return false;
     }
 
     inBounds() {
